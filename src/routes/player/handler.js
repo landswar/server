9b189063d5722a063dda49783a1f68dcb83be6ea
clement <clement22860@gmail.com>
2017-05-09 20:06:23 +0200

    commit feature/jwt

--- src/routes/player/handler.js
+++ src/routes/player/handler.js
@@ -4,6 +4,56 @@ const Player = require('../../models/player');
 /* eslint-disable valid-jsdoc */
 
 /**
+ * @api {get} /players Request every Player.
+ * @apiName getPlayers
+ * @apiGroup Player
+ *
+ * @apiSuccess (200) {Object[]} players List of Players.
+ * @apiSuccess (200) {Number} players.id Player unique ID.
+ * @apiSuccess (200) {String} players.email Email of the player.
+ * @apiSuccess (200) {String} players.nickname Nickname of the player.
+ */
+exports.getPlayers = async function (request, reply) {
+	try {
+		const players = await Player.getAll(['id', 'email', 'nickname']);
+
+		reply(players);
+	} catch (error) {
+		logger.error(error);
+		reply(Boom.badImplementation());
+	}
+};
+
+/**
+ * @api {get} /player/:id Request Player informations.
+ * @apiName getPlayer
+ * @apiGroup Player
+ *
+ * @apiParam {Number} id Player unique ID.
+ *
+ * @apiSuccess (200) {Number} id Player unique ID.
+ * @apiSuccess (200) {String} email Email of the player.
+ * @apiSuccess (200) {String} nickname Nickname of the player.
+ *
+ * @apiError (404) PlayerNotFound The id of the Room was not found.
+ */
+exports.getPlayer = async function (request, reply) {
+	try {
+		const player = await Player.get({ id: request.params.id }, ['id', 'email', 'nickname']);
+
+		if (!player) {
+			reply(Boom.notFound());
+			return;
+		}
+
+		reply(player);
+	} catch (error) {
+		logger.error(error);
+		reply(Boom.badImplementation());
+	}
+};
+
+/**
  * @api {post} /player Create a Player.
  * @apiName postPlayer
  * @apiGroup Player
@@ -27,6 +77,7 @@ exports.createPlayer = async function (request, reply) {
 		}
 
 		const newPlayer = await Player.create({
+			email:    request.payload.email,
 			nickname: request.payload.nickname,
 			password: request.server.methods.lib.crypt(request.payload.password),
 		});
@@ -34,31 +85,8 @@ exports.createPlayer = async function (request, reply) {
 		newPlayer.token = await
 			request.server.methods.lib.createToken(newPlayer.id, newPlayer.nickname);
 
+		newPlayer.password = undefined;
 		reply(newPlayer);
-	} catch (error) {
-		logger.error(error);
-		reply(Boom.badImplementation());
-	}
-};
-
-exports.login = async function (request, reply) {
-	try {
-		const player = await Player.get({
-			nickname: request.payload.nickname,
-		});
-
-		if (!player) {
-			reply(Boom.badRequest('Nickname not exist'));
-			return;
-		} else if (player.password !== request.server.methods.lib.crypt(request.payload.password)) {
-			reply(Boom.badRequest('Wrong password'));
-			return;
-		}
-
-		player.token = await
-			request.server.methods.lib.createToken(player.id, player.nickname);
-
-		reply(player);
 	} catch (error) {
 		logger.error(error);
 		reply(Boom.badImplementation());
