b278d89bcf9e97f9f1098e62c5b9153de65fcefe
Ludovic Lerus <lerus_l@epitech.eu>
2017-06-16 22:39:06 +0200

    test: Error 404 added for /ground/:id

--- test/ground.js
+++ test/ground.js
@@ -1,10 +1,14 @@
+const Boom = require('boom');
 const test = require('ava');
+const Promise = require('bluebird');
+
+global.Promise = Promise;
+
+require('./before');
 
 const GroundPlugin = require('../src/routes/ground/index');
 const Helper = require('./Helper');
 const LandsWarDatabase = require('../src/models/LandsWarDatabase');
-
-require('./before');
 
 const helper = new Helper();
 
@@ -23,5 +27,11 @@ test('/grounds GET', (t) =>
 test('/grounds/1 GET', (t) =>
 	helper.server.inject('/grounds/1').then((res) => {
 		t.deepEqual(res.result, LandsWarDatabase.GROUNDS[0]);
+	})
+);
+
+test('/grounds/1337 GET => 404', (t) =>
+	helper.server.inject('/grounds/1337').then((res) => {
+		t.deepEqual(res.result, Boom.notFound().output.payload);
 	})
 );
