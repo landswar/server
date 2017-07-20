2fd1970a7d0185415a1ce6b3f4505b22277c85b7
Ludovic Lerus <lerus_l@epitech.eu>
2017-07-20 10:20:27 +0200

    docs updated

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-07-11T21:01:56.095Z",
+    "time": "2017-07-20T08:20:16.221Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
