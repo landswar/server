c2139feb8f3736f082ff3e3950c9c5adb1aad901
Ludovic Lerus <lerus_l@epitech.eu>
2017-08-24 17:15:09 +0200

    fix mysql url env

--- src/models/bookshelf.js
+++ src/models/bookshelf.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const knex = require('knex')({
 	client:           'mysql',
-	connection:       process.env.DB_HOST || `mysql://landswar:landswar@127.0.0.1:3306/${env === 'test' ? 'landswar_test' : 'landswar'}`,
+	connection:       process.env.MYSQL_URL || `mysql://landswar:landswar@127.0.0.1:3306/${env === 'test' ? 'landswar_test' : 'landswar'}`,
 	useNullAsDefault: true,
 });
 
