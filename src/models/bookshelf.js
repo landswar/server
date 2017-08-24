80286d8bdf1e8d61235198ce06a2e5faba4d3b9f
Ludovic Lerus <lerus_l@epitech.eu>
2017-08-24 23:10:31 +0200

    travis mysql

--- src/models/bookshelf.js
+++ src/models/bookshelf.js
@@ -1,8 +1,13 @@
 const env = process.env.NODE_ENV || 'development';
+let databaseUrl = process.env.MYSQL_URL || `mysql://landswar:landswar@127.0.0.1:3306/${env === 'test' ? 'landswar_test' : 'landswar'}`;
+
+if (process.env.TRAVIS === 'true') {
+	databaseUrl = 'mysql://root@127.0.0.1:3306/landswar_test';
+}
 
 const knex = require('knex')({
 	client:           'mysql',
-	connection:       process.env.MYSQL_URL || `mysql://landswar:landswar@127.0.0.1:3306/${env === 'test' ? 'landswar_test' : 'landswar'}`,
+	connection:       databaseUrl,
 	useNullAsDefault: true,
 });
 
